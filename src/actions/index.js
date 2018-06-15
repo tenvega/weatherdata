4594daf21021a59165b8694159157801a1b8d8e9
Enoc <etorra@gmail.com>
2018-06-15 16:37:23 -0400

    fetch is working

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '89b6d701c4f91cc3d3254c8c1a609f9e';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-
-export default class SearchBar extends Component{
+import { connect } from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {fetchWeather} from '../actions/index';
+class SearchBar extends Component{
   constructor(props){
     super(props);
 
     this.state ={ term: ''};
 
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 onInputChange(event){
   console.log(event.target.value);
@@ -17,6 +20,8 @@ onFormSubmit(event){
   event.preventDefault();
 
   //We need to go fetch weather data
+  this.props.fetchWeather(this.state.term);
+  this.setState ({ term: '' });
 }
 
   render() {
@@ -33,3 +38,9 @@ onFormSubmit(event){
     );
   }
 }
+
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
